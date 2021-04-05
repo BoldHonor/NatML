98b68d3ffed95b57454db51c184d8a71144ba020
Yusuf Olokoba <olokobayusuf@gmail.com>
2021-04-04 21:19:58 -0400

    Implementing image feature and inference on macOS

--- Runtime/Vision/MLClassifier.cs
+++ Runtime/Vision/MLClassifier.cs
@@ -7,7 +7,6 @@ namespace NatSuite.ML.Vision {
 
     using System.IO;
     using System.Linq;
-    using System.Runtime.InteropServices;
     using System.Threading.Tasks;
     using Features.Types;
     using Internal;
@@ -25,32 +24,49 @@ namespace NatSuite.ML.Vision {
         /// </summary>
         /// <param name="path">Path to ONNX model.</param>
         /// <param name="labels">List of labels which the classifier outputs.</param>
-        public MLClassifier (string path, string[] labels) : base(path) => this.labels = labels;
+        public MLClassifier (string path, string[] labels) : base(path) {
+            this.labels = labels;
+            // Allocate logits
+            var outputShape = ((MLArrayType)this.outputs[0]).shape;
+            this.classes = outputShape.Aggregate(1, (a, b) => a * b);
+        }
 
         /// <summary>
         /// </summary>
         /// <param name="input"></param>
         /// <returns></returns>
-        public (string label, float confidence) Classify (MLFeature input) => Predict(input).First();
+        public unsafe (string label, float confidence) Classify (MLFeature input) {
+            // Predict
+            var inputFeature = input.CreateNMLFeature(this.inputs[0]);
+            var outputFeature = Predict(inputFeature)[0];
+            // Find label
+            var logits = (float*)outputFeature.FeatureData();
+            var argMax = 0;
+            for (var i = 1; i < classes; ++i)
+                if (logits[i] > logits[argMax])
+                    argMax = i;
+            // Return
+            return (labels[argMax], logits[argMax]);
+        }
 
         /// <summary>
         /// </summary>
         /// <param name="inputs"></param>
         /// <returns></returns>
-        public override (string label, float confidence)[] Predict (params MLFeature[] inputs) {
+        public unsafe override (string label, float confidence)[] Predict (params MLFeature[] inputs) { // Slower than ::Classify
             // Predict
             var inputFeature = inputs[0].CreateNMLFeature(this.inputs[0]);
             var outputFeature = Predict(inputFeature)[0];
             // Copy logits
-            var outputShape = ((MLArrayType)this.outputs[0]).shape;
-            var classCount = outputShape.Aggregate(1, (a, b) => a * b);
-            var logits = new float[classCount];
-            Marshal.Copy(outputFeature.FeatureData(), logits, 0, logits.Length);
+            var logits = (float*)outputFeature.FeatureData();
+            var pairs = new (string, float l)[classes];
+            for (var i = 0; i < classes; ++i)
+                pairs[i] = (labels[i], logits[i]);
             // Release
             inputFeature.ReleaseFeature();
             outputFeature.ReleaseFeature();
             // Order descending
-            return logits.Select((l, i) => (labels[i], l)).OrderByDescending(c => c.l).ToArray();
+            return pairs.OrderByDescending(c => c.l).ToArray();
         }
 
         /// <summary>
@@ -58,6 +74,12 @@ namespace NatSuite.ML.Vision {
         /// <param name="relativePath"></param>
         /// <returns></returns>
         public static async Task<string[]> LoadLabelsFromStreamingAssets (string relativePath) => File.ReadAllLines(await MLModelUtility.ModelPathFromStreamingAssets(relativePath));
+        #endregion
+
+
+        #region --Operations--
+
+        private readonly int classes;
         #endregion
     }
 }
